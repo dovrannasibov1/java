c771aacc8a70146d7d0b43e0efd19eb714e4fc24
dovrannasibov1 <dovrannesibov@Gmail.com>
2023-10-02 18:33:32 +0400

    java

---
+++ java.js
@@ -0,0 +1,68 @@
+// var result = prompt("imtahan neticeni yaz") 
+// if(result >=80 && result <= 100) {
+//     console.log("qiymət A");
+//     alert("qiymət A");
+// } else if (result >= 70 && result < 80){
+//     console.log("b aldin");
+//     alert("b aldin");
+// }else if (result >= 60 && result < 70){
+//     console.log("c aldin");
+//     alert("c aldin");
+// }else if (result >= 50 && result < 60){
+//     console.log("d aldin");
+//     alert("d aldin");
+// }else if (result >= 0 && result < 50){
+//     console.log("f aldin");
+//     alert("f aldin");
+// } else{
+//     console.log("daxil etdiyin netice duzgun deyil")
+//     alert("daxil etdiyin netice duzgun deyil");
+// }
+
+// console.log(imtahanNeticesi)
+
+var q1 = prompt ("Eacamp school harda yerləşir CAVAB-jale plaza") ;
+if (q1 == "jale plaza") {
+    // console.log("aferin");
+    alert("aferin");   
+}else{
+    console.log("yalnış Cavab")
+        alert("yalnış Cavab");
+}
+var q2 = prompt ("2-ci Qarabağ Müharibəsi nə vaxt olub");
+
+if (q2 == "27 sentyabr") {
+
+console.log("aferin");
+alert("aferin")
+}else{
+    console.log("yalnış Cavab")
+        alert("yalnış Cavab");
+}
+var q3 = prompt ("Sevincin ad günü tortu xoşuna gəldi? CAVAB-çox bəyəndim")
+if (q3 =="çox bəyəndim") {
+    console.log("mən Sevinc: çox təşəkkür edirəm")
+    alert("mən Sevinc: çox təşəkkür edirəm")
+}
+
+var q3 = prompt ("razinə yaxın metronun adı nədir?");
+if (q3 == "neftçilər"){
+    console.log("aferin");
+    alert("aferin")
+}else{
+    console.log("yalnış Cavab")
+        alert("yalnış Cavab");
+}
+
+var q4 = prompt ("Əhmədliyə ən yaxın metronun adı nədir?");
+if (q4 == "xalqlar"){
+    console.log("aferin");
+    alert("aferin") 
+}else{
+    console.log("yalnış Cavab")
+        alert("yalnış Cavab");
+}
+
+
+        
+
